d42d9516f21d3d9dd46dbe9d57ce8a97a28cc40c
Thushan-Shamendra <shamendrat@gmail.com>
2026-02-22 21:04:51 +0530

    Creating a Web Server

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("NodeJS Lab 03 Started Successfully!");
+const fs = require('fs'); 
+fs.readFile('file.txt', 'utf8', function (err, data) { 
+if (err) throw err; 
+console.log(data); 
+}); 
+ 
+//Write
+const fs = require('fs');
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
+
+//Web server
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
