8d4995c3a54e2908a5c23a9756f8739c2979f4be
Thushan-Shamendra <shamendrat@gmail.com>
2026-02-22 21:06:49 +0530

    Making HTTP Request

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("NodeJS Lab 03 Started Successfully!");
+const fs = require('fs'); 
+fs.readFile('file.txt', 'utf8', function (err, data) { 
+if (err) throw err; 
+console.log(data); 
+}); 
+ 
+//Write
+const fs = require('fs');
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
+
+//Web server
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+//Making HTTP Request
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
